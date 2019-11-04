0a60cc092b6acda1d182ba54187d4fc7534094dc
AmeliaAnn <53219102+AmeliaAnn@users.noreply.github.com>
2019-11-04 14:30:10 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@ package com.exersise13;
 import java.util.Scanner;
 
 
-public class Main{
+public class Main {
 
    public static void main(String args[]) {
     
@@ -17,19 +17,19 @@ public class Main{
       
       System.out.println( "Введите вторую строку: " );
       String s2 = in.nextLine();
-     int len2 = s2.length();
+      int len2 = s2.length();
      
      if (len1 > len2)  {
       System.out.println(s1);
-   }
-     if (len1 < len2)  {
+   } else {
+     if (len1 < len2)  
       System.out.println(s2);
+     else {
+       System.out.println("Строки равны");
+     }
+     
 
-   }
-     if (len1 == len2) {
-     System.out.println("Дины строк равны");
+       }
+         
      }
  }
-     
-     
-     }
